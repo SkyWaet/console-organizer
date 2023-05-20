2a4d23de8026e03f3a0f1eb9a2dfe0a58bc03b80
SkyWaet <SkyWaet@yandex.ru>
2023-05-20 20:02:29 +0300

    XML utils added

---
+++ src/main/java/xml/XmlUtils.java
@@ -0,0 +1,166 @@
+package xml;
+
+import model.Person;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class XmlUtils {
+    private XmlUtils() {
+    }
+
+    public static Map<Integer, Person> readAllDataFrom(String fileName) {
+        try {
+            Map<Integer, Person> persons = new HashMap<>();
+
+            Document doc = parseDocument(fileName);
+            Element root = doc.getDocumentElement();
+            NodeList nodes = root.getChildNodes();
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                if (node instanceof Element e) {
+                    Person person = getPersonFromElement(e);
+                    persons.put(person.getServiceNumber(), person);
+                }
+            }
+            return persons;
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void addRecordTo(String fileName, Person person) {
+        try {
+            Document doc = parseDocument(fileName);
+            Element root = doc.getDocumentElement();
+            root.appendChild(getElementFromPerson(doc, person));
+            writeToFile(doc, fileName);
+        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void updateRecord(String fileName, Person person) {
+        try {
+            Document doc = parseDocument(fileName);
+            Element root = doc.getDocumentElement();
+            Element newVersion = getElementFromPerson(doc, person);
+            root.replaceChild(newVersion, findOldVersion(root, person.getServiceNumber()));
+            writeToFile(doc, fileName);
+        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Document parseDocument(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        DocumentBuilder b = f.newDocumentBuilder();
+        return b.parse(new File(fileName));
+    }
+
+    public static void writeToFile(Document document, String fileName) throws TransformerException {
+        DOMSource source = new DOMSource(document);
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer();
+        StreamResult result = new StreamResult(fileName);
+        transformer.transform(source, result);
+    }
+
+
+    public static Element getElementFromPerson(Document doc, Person person) {
+        Element personRoot = doc.createElement("person");
+        personRoot.setAttribute("serviceNumber", person.getServiceNumber().toString());
+
+        Element surname = doc.createElement("surname");
+        surname.appendChild(doc.createTextNode(person.getSurname()));
+        personRoot.appendChild(surname);
+
+        Element name = doc.createElement("name");
+        name.appendChild(doc.createTextNode(person.getName()));
+        personRoot.appendChild(name);
+
+        Element patronymic = doc.createElement("patronymic");
+        patronymic.appendChild(doc.createTextNode(person.getPatronymic()));
+        personRoot.appendChild(patronymic);
+
+        Element position = doc.createElement("position");
+        position.appendChild(doc.createTextNode(person.getPosition()));
+        personRoot.appendChild(position);
+
+        Element organization = doc.createElement("organization");
+        organization.appendChild(doc.createTextNode(person.getOrganization()));
+        personRoot.appendChild(organization);
+
+        Element email = doc.createElement("email");
+        email.appendChild(doc.createTextNode(person.getEmail()));
+        personRoot.appendChild(email);
+
+        Element phoneNumbers = doc.createElement("phoneNumbers");
+        phoneNumbers.appendChild(doc.createTextNode(String.join(", ", person.getPhoneNumbers())));
+        personRoot.appendChild(phoneNumbers);
+
+        return personRoot;
+
+    }
+
+    private static Element findOldVersion(Element root, Integer serviceNumber) {
+        NodeList childNodes = root.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            if (child instanceof Element e && e.getAttribute("serviceNumber").equals(serviceNumber.toString())) {
+                return e;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static Person getPersonFromElement(Element element) {
+
+        Person.PersonBuilder builder = Person.builder();
+        Integer serviceNumber = Integer.parseInt(element.getAttribute("serviceNumber"));
+        builder.serviceNumber(serviceNumber);
+
+        NodeList childNodes = element.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            if (child instanceof Element e) {
+                switch (e.getTagName()) {
+                    case "name" -> builder.name(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "surname" -> builder.surname(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "patronymic" -> builder.patronymic(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "position" -> builder.position(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "organization" -> builder.organization(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "email" -> builder.email(getTextFromTextNode((Text) e.getFirstChild()));
+                    case "phoneNumbers" -> builder.phoneNumbers(getListFromTextNode((Text) e.getFirstChild()));
+                    default -> System.out.println("Unknown tag " + e.getTagName());
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    private static String getTextFromTextNode(Text node) {
+        return node.getData().trim();
+    }
+
+    private static List<String> getListFromTextNode(Text node) {
+        return Arrays.stream(node.getData().trim().split(", ")).toList();
+    }
+
+}
---
+++ src/test/java/xml/XmlUtilsTest.java
@@ -0,0 +1,115 @@
+package xml;
+
+import model.Person;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class XmlUtilsTest {
+    private static final String FILE_PATH = "./src/test/resources/Storage.xml";
+    private final Person testPerson = Person.builder()
+            .serviceNumber(1)
+            .surname("Иванов")
+            .name("Иван")
+            .patronymic("Иванович")
+            .position("Разработчик")
+            .organization("Рога и Копыта")
+            .email("ivanov@email")
+            .phoneNumbers(List.of("+7123456789", "+7000000000"))
+            .build();
+
+    @BeforeEach
+    public void init() throws TransformerException, ParserConfigurationException, IOException, SAXException {
+        Files.createFile(Path.of(FILE_PATH));
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Element root = document.createElement("persons");
+        document.appendChild(root);
+        Element personAsElement = XmlUtils.getElementFromPerson(document, testPerson);
+        root.appendChild(personAsElement);
+        XmlUtils.writeToFile(document, FILE_PATH);
+    }
+
+    @AfterEach
+    public void clearFile() throws IOException {
+        Files.delete(Path.of(FILE_PATH));
+    }
+
+    @Test
+    public void shouldReadData() {
+
+        Map<Integer, Person> read = XmlUtils.readAllDataFrom(FILE_PATH);
+
+        assertThat(read).asInstanceOf(InstanceOfAssertFactories.map(Integer.class, Person.class))
+                .containsKey(testPerson.getServiceNumber())
+                .containsValue(testPerson);
+    }
+
+    @Test
+    public void shouldWriteData() {
+        Person expected = Person.builder()
+                .serviceNumber(2)
+                .surname("Петров")
+                .name("Петр")
+                .patronymic("Петрович")
+                .position("Разработчик")
+                .organization("Рога и Копыта")
+                .email("petrov@email")
+                .phoneNumbers(List.of("+7876543210", "+7000000000"))
+                .build();
+
+        XmlUtils.addRecordTo(FILE_PATH, expected);
+
+        Map<Integer, Person> read = XmlUtils.readAllDataFrom(FILE_PATH);
+
+        assertThat(read).asInstanceOf(InstanceOfAssertFactories.map(Integer.class, Person.class))
+                .hasSize(2)
+                .containsKey(expected.getServiceNumber())
+                .containsValue(expected);
+    }
+
+    @Test
+    public void shouldUpdateExistingData() {
+
+        Person expected = Person.builder()
+                .serviceNumber(testPerson.getServiceNumber())
+                .surname(testPerson.getSurname())
+                .name(testPerson.getName())
+                .patronymic(testPerson.getPatronymic())
+                .position("Старший разработчик")
+                .organization(testPerson.getOrganization())
+                .email(testPerson.getEmail())
+                .phoneNumbers(testPerson.getPhoneNumbers())
+                .build();
+
+        Map<Integer, Person> beforeUpdate = XmlUtils.readAllDataFrom(FILE_PATH);
+
+        assertThat(beforeUpdate).asInstanceOf(InstanceOfAssertFactories.map(Integer.class, Person.class))
+                .containsKey(expected.getServiceNumber())
+                .doesNotContainValue(expected);
+
+        XmlUtils.updateRecord(FILE_PATH, expected);
+
+        Map<Integer, Person> afterUpdate = XmlUtils.readAllDataFrom(FILE_PATH);
+        assertThat(afterUpdate).asInstanceOf(InstanceOfAssertFactories.map(Integer.class, Person.class))
+                .containsKey(expected.getServiceNumber())
+                .containsValue(expected);
+
+    }
+
+
+}
